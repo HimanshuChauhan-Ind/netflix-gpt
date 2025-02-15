b46bc20b6db1e1198cb526317587d126f59020ce
Himanshu Chauhan <him97cha@gmail.com>
2025-02-15 18:24:19 +0530

    Fixed the alignment of Login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,38 +18,40 @@ const Login = () => {
           src={BG_URL}
         />
       </div>
-      <div className="w-xl p-10 bg-gray-950/50 absolute top-1/4 left-1/4 text-white">
-        <div className="text-3xl">{signedUp ? "Sign In" : "Sign Up"}</div>
-        <form>
-          {!signedUp && (
+      <div className="absolute inset-0 flex items-center justify-center">
+        <div className="w-xl p-10 bg-gray-950/50 top-1/4 left-1/4 text-white">
+          <div className="text-3xl">{signedUp ? "Sign In" : "Sign Up"}</div>
+          <form>
+            {!signedUp && (
+              <input
+                className="border border-b-white w-full p-2 my-4 rounded-md"
+                type="text"
+                name="fullName"
+                placeholder="Full Name"
+              />
+            )}
             <input
               className="border border-b-white w-full p-2 my-4 rounded-md"
               type="text"
-              name="fullName"
-              placeholder="Full Name"
+              name="email"
+              placeholder="Email or Username"
             />
-          )}
-          <input
-            className="border border-b-white w-full p-2 my-4 rounded-md"
-            type="text"
-            name="email"
-            placeholder="Email or Username"
-          />
-          <input
-            className="border border-b-white w-full p-2 my-4 rounded-md"
-            type="text"
-            name="password"
-            placeholder="Password"
-          />
-          <button className="bg-red-400 w-full my-4 p-2 rounded-md">
-            {signedUp ? "Sign In" : "Sign Up"}
-          </button>
-          <div className="cursor-pointer" onClick={toggleSignUp}>
-            {signedUp
-              ? "New to Netflix? Sign up now."
-              : "Alredy a User? Sign in now."}
-          </div>
-        </form>
+            <input
+              className="border border-b-white w-full p-2 my-4 rounded-md"
+              type="text"
+              name="password"
+              placeholder="Password"
+            />
+            <button className="bg-red-400 w-full my-4 p-2 rounded-md">
+              {signedUp ? "Sign In" : "Sign Up"}
+            </button>
+            <div className="cursor-pointer" onClick={toggleSignUp}>
+              {signedUp
+                ? "New to Netflix? Sign up now."
+                : "Alredy a User? Sign in now."}
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
