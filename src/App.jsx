2707d7e5d608cec6660aac4b2be411abeca1dab3
Himanshu Chauhan <him97cha@gmail.com>
2025-02-15 12:07:17 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-amber-400">
+        This seems to be a new start!!!
+      </h1>
+    </>
+  );
+}
+
+export default App;
