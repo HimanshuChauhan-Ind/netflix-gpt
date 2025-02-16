37f9ba5fa3cef722f95ce4d33c85ddaf6d111aa8
Himanshu Chauhan <him97cha@gmail.com>
2025-02-16 11:24:43 +0530

    Added the form validation

---
+++ src/assets/validateForm.js
@@ -0,0 +1,21 @@
+const validateForm = (email, password) => {
+  const emailCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+    email
+  );
+  const passwordCheck =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+      password
+    );
+
+  if (!emailCheck) {
+    return "Email is not valid";
+  }
+
+  if (!passwordCheck) {
+    return "Password is not valid";
+  }
+
+  return true;
+};
+
+export default validateForm;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { BG_URL } from "../assets/constants";
 import Header from "./Header";
+import validateForm from "../assets/validateForm";
 
 const Login = () => {
   const [signedUp, setSignedUp] = useState(true);
+  const [validationMsg, setValidationMsg] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
   const toggleSignUp = () => {
     setSignedUp(!signedUp);
+  };
+
+  const handleSignup = () => {
+    console.log(email.current.value);
+    console.log(password.current.value);
+    const validation = validateForm(
+      email.current.value,
+      password.current.value
+    );
+    setValidationMsg(validation);
   };
 
   return (
@@ -21,7 +35,7 @@ const Login = () => {
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="w-xl p-10 bg-gray-950/50 top-1/4 left-1/4 text-white">
           <div className="text-3xl">{signedUp ? "Sign In" : "Sign Up"}</div>
-          <form>
+          <form onSubmit={(e) => e.preventDefault()}>
             {!signedUp && (
               <input
                 className="border border-b-white w-full p-2 my-4 rounded-md"
@@ -31,18 +45,24 @@ const Login = () => {
               />
             )}
             <input
+              ref={email}
               className="border border-b-white w-full p-2 my-4 rounded-md"
               type="text"
               name="email"
               placeholder="Email or Username"
             />
             <input
+              ref={password}
               className="border border-b-white w-full p-2 my-4 rounded-md"
-              type="text"
+              type="password"
               name="password"
               placeholder="Password"
             />
-            <button className="bg-red-400 w-full my-4 p-2 rounded-md">
+            <p className="text-red-500">{validationMsg}</p>
+            <button
+              onClick={handleSignup}
+              className="bg-red-400 w-full my-4 p-2 rounded-md"
+            >
               {signedUp ? "Sign In" : "Sign Up"}
             </button>
             <div className="cursor-pointer" onClick={toggleSignUp}>
